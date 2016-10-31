64f79c0dd1bd2f3754f5ab1d4852cb12fdd37e03
Jón Tryggvi Unnarsson <dasfunkyt@gmail.com>
2016-10-31 16:29:50 +0000

    working on frontage

--- www/index.php
+++ www/index.php
@@ -2,26 +2,45 @@
 	include('admin/includes/config.php');
 	include('includes/header.php');
 ?>
-<div class="container-fluid">
-	<div class="row">
 
-		<nav class="navbar navbar-light bg-faded">
-		<?php createUserNavigation('userNav'); ?>
-		</nav>
-	</div>
-	<div class="row">
-		<div class="col-md-3 p-a-3">
-			
-		</div>
-		<div class="col-md-9 p-a-3">
-			<div class="row">
-				<nav class="breadcrumb">
-				  <a class="breadcrumb-item" href="#">Home</a>
-				  <span class="breadcrumb-item active" href="#">Dashboard</span>
-				</nav>
+
+
+		<nav class="mainNav">
+
+			<div class="wrap">
+
+				<a class="" href="#">
+	        <img alt="Brand" src="img/icons/burger.svg">
+	      </a>
+				<a class="" href="#">
+					<img alt="Brand" src="img/icons/logo.svg">
+				</a>
+	      <a class="" href="#">
+	        <img alt="Brand" src="img/icons/user.svg">
+	      </a>
+
 			</div>
-		</div>
+	</nav>
+<div class="container-fluid row">
+	<section class="texture col-md-7">
+		<h1>How Shitty is your day?</h1>
+		<h2>Take the poop test to find out...</h2>
+		<form class="texture-form" action="" method="post">
+			<input type="button" name="name" value="0">
+			<input type="button" name="name" value="1">
+			<input type="button" name="name" value="2">
+			<input type="button" name="name" value="3">
+			<input type="button" name="name" value="4">
+			<input type="button" name="name" value="5">
+			<input type="button" name="name" value="6">
+			<input type="button" name="name" value="7">
+			<input type="button" name="name" value="8">
+			<input type="button" name="name" value="9">
+		</form>
+	</section>
+	<section class="shade col-md-5">
 
-	</div>
+	</section>
 </div>
+
 <?php include('includes/footer.php')  ?>
