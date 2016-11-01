2d06d1fa3d899b54f3e551395fe54dd9ef4ce5e7
Jón Tryggvi Unnarsson <dasfunkyt@gmail.com>
2016-11-01 10:37:07 +0000

    frontpage vesen

--- www/index.php
+++ www/index.php
@@ -3,12 +3,8 @@
 	include('includes/header.php');
 ?>
 
-
-
 		<nav class="mainNav">
-
 			<div class="wrap">
-
 				<a class="" href="#">
 	        <img alt="Brand" src="img/icons/burger.svg">
 	      </a>
@@ -18,14 +14,12 @@
 	      <a class="" href="#">
 	        <img alt="Brand" src="img/icons/user.svg">
 	      </a>
-
 			</div>
 	</nav>
 <div class="container-fluid row">
 	<section class="texture col-md-7">
 		<h1>How Shitty is your day?</h1>
 		<h2>Take the poop test to find out...</h2>
-		<h1>Texture</h1>
 		<form class="texture-form" action="" method="post">
 			<input type="button" name="hard-lumps" value="1">
 			<input type="button" name="sausage-soft" value="2">
@@ -35,21 +29,24 @@
 			<input type="button" name="fluffy" value="9">
 			<input type="button" name="soft-sticks" value="10">
 			<input type="button" name="empty" value="11">
+
 		</form>
 	</section>
 	<section class="shade col-md-5">
-		<h1>Carry on...</h1>
-		<h2>Choose your color...</h2>
-		<h1>shade</h1>
-		<form class="texture-form" action="" method="post">
-			<input type="button" name="brown" value="1">
-			<input type="button" name="green" value="2">
-			<input type="button" name="yellow" value="3">
-			<input type="button" name="black" value="4">
-			<input type="button" name="white" value="5">
-			<input type="button" name="red" value="6">
-		</form>
-	</section>
+-		<h1>Carry on...</h1>
+-		<h2>Choose your color...</h2>
+-		<h1>shade</h1>
+-		<form class="texture-form" action="" method="post">
+-			<input type="button" name="brown" value="1">
+-			<input type="button" name="green" value="2">
+-			<input type="button" name="yellow" value="3">
+-			<input type="button" name="black" value="4">
+-			<input type="button" name="white" value="5">
+-			<input type="button" name="red" value="6">
+-		</form>
++
+ 	</section>
+
 </div>
 
 <?php include('includes/footer.php')  ?>
