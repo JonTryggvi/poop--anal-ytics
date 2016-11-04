2866bf09fa61e7989e1f9078836a4b1851e957f7
Ása Einarsdóttir <alsaugran@gmail.com>
2016-11-04 14:31:59 +0000

    Modal start

--- www/assets/js/script-nav.js
+++ www/assets/js/script-nav.js
@@ -1,6 +1,15 @@
+/* Nav */
+
+
 $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
 });
 
 console.log("Halló Kalló Bimbó");
+
+/* User Modal Window */
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+});
