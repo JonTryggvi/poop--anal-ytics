d7d3a290a308762fbcf59dd03ba6d26b8fb841cd
Jón Tryggvi Unnarsson <dasfunkyt@gmail.com>
2016-11-08 15:00:00 +0000

    test komin og far inní töflur úr diary, dasboard og frontpage

--- www/admin/includes/diaryTest.php
+++ www/admin/includes/diaryTest.php
@@ -5,34 +5,34 @@
 			<h1>How Shitty is your day?</h1>
 			<h2>Take the poop test to find out...</h2>
 			<h1>texture</h1>
-			<?php textureRadios(); ?>
+			<?php textureRadiosDiary(); ?>
 		</section>
 		<section class="shade col-md-4 pull-right">
 			<h1>Carry on...</h1>
 			<h2>Choose your color...</h2>
 			<h1>shade</h1>
-			<?php shadeRadios(); ?>
+			<?php shadeRadiosDiary(); ?>
 
 
 	 	</section>
-		<section>
-			<input type="text" name="diary-title" value="">
-			<input type="text" name="diary-content" value="">
-			<input id="formSubmitD" type="submit" name="submit" value="submit">
+		<section class="diary">
+			<input class="diary-title" type="text" name="diary-title" value="">
+			<input class="diary-content" type="text" name="diary-content" value="">
+			<input class="diary-submit" id="formSubmitD" type="submit" name="submit-diary" value="submit">
 		</section>
 
 
 		<section class="col-md-12 row test-results ">
 			<div class="col-md-8 centerme row">
 				<?php
-					if (isset($_POST['texture']) && isset($_POST['shade']) ) {
-						showResultIcons($texture, $shade);
+					if (isset($_POST['texture-diary']) && isset($_POST['shade-diary']) ) {
+						showResultIconsDiary($texture, $shade);
 					}
 				?>
 
 			<?php
-				if (isset($_POST['texture']) && isset($_POST['shade']) ) {
-					showTextureResult($texture, $shade);
+				if (isset($_POST['texture-diary']) && isset($_POST['shade-diary']) ) {
+					showTextureResultDiary($texture, $shade);
 				}
 			?>
 			</div>
